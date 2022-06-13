1dd05de1905d3470deb272464618e30bcf190897
RAFLI <rafli1162003@gmail.com>
2022-06-13 18:24:51 +0700

    Major Changes

--- say_hello.go
+++ say_hello.go
@@ -1,7 +1,7 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello, World!"
+func SayHello(name string) string {
+	return "Hello, " + name + "!" +"Are You Ready?"
 }
 
 func SayYourName(name string) string {
